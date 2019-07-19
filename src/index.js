c81fd850b294296c47dcddc88a59b0f1f758db96
julio baltazar <juliocesar.jcba@gmail.com>
2019-07-19 15:45:51 -0300

    finalizacao dos exercicios

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import PrimeiroComponente from './componentes/PrimeiroComponente'
-import CompA, { CompB as B } from './componentes/DoisComponentes'
-import MultiElementos from './componentes/MultiElementos'
-import FamiliaBaltazar from './componentes/FamiliaBaltazar'
-import Familia from './componentes/Familia'
-import Membro from './componentes/Membro'
-import ComponenteComFuncao from './componentes/ComponenteComFuncao'
-import Pai from './componentes/Pai'
+// import PrimeiroComponente from './componentes/PrimeiroComponente'
+// import CompA, { CompB as B } from './componentes/DoisComponentes'
+// import MultiElementos from './componentes/MultiElementos'
+// import FamiliaBaltazar from './componentes/FamiliaBaltazar'
+// import Familia from './componentes/Familia'
+// import Membro from './componentes/Membro'
+// import ComponenteComFuncao from './componentes/ComponenteComFuncao'
+// import Pai from './componentes/Pai'
+// import ComponenteClasse from './componentes/ComponenteClasse'
+// import Contador from './componentes/Contador'
+import Hook from './componentes/Hook'
 
 const elemento = document.getElementById('root');
 ReactDOM.render(
     <div>
-        <Pai />
+        <Hook />
+        {/* <Contador numeroInicial={10}/> */}
+        {/* <ComponenteClasse valor="Ola Classe com props"/> */}
+        {/* <Pai />
         <ComponenteComFuncao />
         <PrimeiroComponente valor="Loreninha" />
         <CompA valor="Ola" />
@@ -26,7 +32,7 @@ ReactDOM.render(
         <Familia sobrenome="Pinto">
             <Membro nome="Ana" sobrenome="Pinto"/>
             <Membro nome="Mariana" sobrenome="Pinto"/>
-        </Familia>
+        </Familia> */}
     </div>
     , elemento)
 
