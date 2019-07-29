63b7e432c2bd8c34b1a3abdc17dae479342d01d7
Julio Baltazar <julio.baltazar@ifactory.com.br>
2019-07-29 11:22:58 -0300

    exemplo redux

---
+++ src/index-.js
@@ -0,0 +1,48 @@
+// import ReactDOM from 'react-dom';
+// import React from 'react';
+// // import PrimeiroComponente from './componentes/PrimeiroComponente'
+// // import CompA, { CompB as B } from './componentes/DoisComponentes'
+// // import MultiElementos from './componentes/MultiElementos'
+// // import FamiliaBaltazar from './componentes/FamiliaBaltazar'
+// // import Familia from './componentes/Familia'
+// // import Membro from './componentes/Membro'
+// // import ComponenteComFuncao from './componentes/ComponenteComFuncao'
+// // import Pai from './componentes/Pai'
+// // import ComponenteClasse from './componentes/ComponenteClasse'
+// // import Contador from './componentes/Contador'
+// import Hook from './componentes/Hook'
+
+// const elemento = document.getElementById('root');
+// ReactDOM.render(
+//     <div>
+//         <Hook />
+//         {/* <Contador numeroInicial={10}/> */}
+//         {/* <ComponenteClasse valor="Ola Classe com props"/> */}
+//         {/* <Pai />
+//         <ComponenteComFuncao />
+//         <PrimeiroComponente valor="Loreninha" />
+//         <CompA valor="Ola" />
+//         <B valor="Alou" />
+//         <MultiElementos />
+//         <FamiliaBaltazar />
+//         <Familia sobrenome="Matos">
+//             <Membro nome="Andre" sobrenome="Matos"/>
+//             <Membro nome="Mariana" sobrenome="Matos"/>
+//         </Familia>
+//         <Familia sobrenome="Pinto">
+//             <Membro nome="Ana" sobrenome="Pinto"/>
+//             <Membro nome="Mariana" sobrenome="Pinto"/>
+//         </Familia> */}
+//     </div>
+//     , elemento)
+
+// // const jsx = <h1>olá react!</h1>
+// // ReactDOM.render(jsx, elemento);
+
+// // ReactDOM.render(
+// //     <ul>
+// //         <li>Julio</li>
+// //         <li>Lorena</li>
+// //         <li>Duda</li>
+// //     </ul>
+// //     , elemento)
--- src/index.js
+++ src/index.js
@@ -1,48 +1,16 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-// import PrimeiroComponente from './componentes/PrimeiroComponente'
-// import CompA, { CompB as B } from './componentes/DoisComponentes'
-// import MultiElementos from './componentes/MultiElementos'
-// import FamiliaBaltazar from './componentes/FamiliaBaltazar'
-// import Familia from './componentes/Familia'
-// import Membro from './componentes/Membro'
-// import ComponenteComFuncao from './componentes/ComponenteComFuncao'
-// import Pai from './componentes/Pai'
-// import ComponenteClasse from './componentes/ComponenteClasse'
-// import Contador from './componentes/Contador'
-import Hook from './componentes/Hook'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
+import counterReducer from './ex-redux/counterReducer'
+import Counter from './ex-redux/counter'
 
-const elemento = document.getElementById('root');
+const reducers = combineReducers({
+    counter: counterReducer
+})
+
 ReactDOM.render(
-    <div>
-        <Hook />
-        {/* <Contador numeroInicial={10}/> */}
-        {/* <ComponenteClasse valor="Ola Classe com props"/> */}
-        {/* <Pai />
-        <ComponenteComFuncao />
-        <PrimeiroComponente valor="Loreninha" />
-        <CompA valor="Ola" />
-        <B valor="Alou" />
-        <MultiElementos />
-        <FamiliaBaltazar />
-        <Familia sobrenome="Matos">
-            <Membro nome="Andre" sobrenome="Matos"/>
-            <Membro nome="Mariana" sobrenome="Matos"/>
-        </Familia>
-        <Familia sobrenome="Pinto">
-            <Membro nome="Ana" sobrenome="Pinto"/>
-            <Membro nome="Mariana" sobrenome="Pinto"/>
-        </Familia> */}
-    </div>
-    , elemento)
-
-// const jsx = <h1>olá react!</h1>
-// ReactDOM.render(jsx, elemento);
-
-// ReactDOM.render(
-//     <ul>
-//         <li>Julio</li>
-//         <li>Lorena</li>
-//         <li>Duda</li>
-//     </ul>
-//     , elemento)
+    <Provider store={createStore(reducers)}>
+        <Counter />
+    </Provider>
+, document.getElementById('app'))
